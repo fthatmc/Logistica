c59e854929c58139bc485dc1949f420e9f052e65
fthatmc <136472585+fthatmc@users.noreply.github.com>
2024-06-02 20:35:08 +0300

    .

---
+++ Frontends/Logistica.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Logistica.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
