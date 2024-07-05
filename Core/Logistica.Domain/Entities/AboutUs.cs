45b4e520284f9c72bdc73d4adb28c6e2553da875
fthatmc <136472585+fthatmc@users.noreply.github.com>
2024-07-05 21:20:00 +0300

    .

--- Core/Logistica.Domain/Entities/AboutUs.cs
+++ Core/Logistica.Domain/Entities/AboutUs.cs
@@ -12,5 +12,6 @@ namespace Logistica.Domain.Entities
         public string Title { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
+        public string Image { get; set; }
     }
 }
