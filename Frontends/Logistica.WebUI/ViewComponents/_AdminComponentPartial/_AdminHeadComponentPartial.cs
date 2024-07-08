f9c4ae366a5d32bbf7a3af181ca2fa408c9a8467
fthatmc <136472585+fthatmc@users.noreply.github.com>
2024-07-08 09:55:22 +0300

    .

---
+++ Frontends/Logistica.WebUI/ViewComponents/_AdminComponentPartial/_AdminHeadComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Logistica.WebUI.ViewComponents._AdminComponentPartial
+{
+	public class _AdminHeadComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
